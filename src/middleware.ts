81d25ace93c8c1ffa123b3e12728c3f525c3a5c2
haohanw1-creator <haohanw1@uci.edu>
2026-03-04 12:22:46 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,8 +11,8 @@ export async function middleware(req: NextRequest) {
     {
       cookies: {
         getAll: () => req.cookies.getAll(),
-        setAll: (cookies) => {
-          cookies.forEach(({ name, value, options }) => {
+setAll: (cookies: any[]) => {
+  cookies.forEach(({ name, value, options }) => {
             res.cookies.set(name, value, options);
           });
         },
